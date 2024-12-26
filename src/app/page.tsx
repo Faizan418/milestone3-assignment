484e2497905abbaa72d46535d8bda690ec921108
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-12-26 23:27:11 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
-import Image from "next/image";
 
 export default async function Home() {
   const fetchBlog = `*[_type == 'blog']{
-    _id,
-    title,
+  title,
     heading,
     description,
     image,
-  }`;
+}`;
 
   let blogData = [];
   try {
@@ -17,19 +15,22 @@ export default async function Home() {
   } catch (error) {
     console.error("Failed to fetch blog data:", error);
   }
+  console.log(blogData); 
 
   return (
-    <div>
-      <h1 className="text-center text-4xl font-semibold p-6">Blogging Website</h1>
+    <div className="border">
+      <h1 className="text-center text-4xl font-semibold p-6 mb-10">Blogging Website</h1>
 
+      <div className="main_div">
       {blogData.length > 0 ? (
         blogData.map((post:any) => (
-          <div key={post._id} className="border p-5 hover:shadow-lg transition-shadow duration-300">
-            <Image
+          <div key={post._id} className="border rounded-lg p-4 hover:shadow-lg transition-shadow duration-300 w-96 box">
+            <img
               src={post.image ? urlFor(post.image).url() : "/placeholder.jpg"}
               alt={post.title || "Blog image"}
-              height={200}
-              width={200}
+              height={300}
+              width={400}
+              className="border rounded-lg imag"
             />
             <h2 className="text-2xl font-semibold mb-4">{post.title}</h2>
             <h3 className="text-xl font-semibold mb-2">{post.heading}</h3>
@@ -39,6 +40,7 @@ export default async function Home() {
       ) : (
         <p className="text-center text-gray-500">No blog posts available.</p>
       )}
+      </div>
     </div>
   );
 }
